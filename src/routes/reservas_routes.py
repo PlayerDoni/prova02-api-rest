34ce11544da54934cc06fa3860678087da88c35a
Donovan <donovanteixeiradelima@gmail.com>
2023-11-27 21:54:09 -0300

    fim prova

--- src/routes/reservas_routes.py
+++ src/routes/reservas_routes.py
@@ -10,42 +10,83 @@ from src.models.voos_model import Voo
 
 reservas_router = APIRouter(prefix="/reservas")
 
+@reservas_router.get("/voos/vendas")
+def vendas_voo_com_limite(limite: int = 2):
+    # Lógica da rota aqui
+    pass
 
-@reservas_router.get("/{id_voo}")
-def lista_reservas_voo(id_voo: int):
+@reservas_router.get("/voos")
+def lista_voos():
     with get_session() as session:
-        statement = select(Reserva).where(Reserva.voo_id == id_voo)
-        reservas = session.exec(statement).all()
-        return reservas
+        voos = session.exec(select(Voo)).all()
+        return voos
 
+@reservas_router.post("/voos")
+def cria_voo(voo: Voo):
+    with get_session() as session:
+        session.add(voo)
+        session.commit()
+        session.refresh(voo)
+        return voo
 
 @reservas_router.post("")
 def cria_reserva(reserva: Reserva):
     with get_session() as session:
-        voo = session.exec(select(Voo).where(Voo.id == reserva.voo_id)).first()
-
-        if not voo:
+        # Verifique se já existe uma reserva para o mesmo documento
+        existente = session.exec(select(Reserva).where(Reserva.documento == reserva.documento)).first()
+        if existente:
             return JSONResponse(
-                content={"message": f"Voo com id {reserva.voo_id} não encontrado."},
-                status_code=404,
+                content={"message": "Já existe uma reserva para este documento."},
+                status_code=400,
             )
 
-        # TODO - Validar se existe uma reserva para o mesmo documento
-
-        codigo_reserva = "".join(
-            [str(random.randint(0, 999)).zfill(3) for _ in range(2)]
-        )
-
-        reserva.codigo_reserva = codigo_reserva
-        session.add(reserva)
-        session.commit()
-        session.refresh(reserva)
-        return reserva
-
+        # Restante da lógica da rota
+        # ...
 
 @reservas_router.post("/{codigo_reserva}/checkin/{num_poltrona}")
 def faz_checkin(codigo_reserva: str, num_poltrona: int):
-    # TODO - Implementar reserva de poltrona
-    pass
+    with get_session() as session:
+        reserva = session.exec(select(Reserva).where(Reserva.codigo_reserva == codigo_reserva)).first()
 
-# TODO - Implementar troca de reserva de poltrona
+        if not reserva:
+            return JSONResponse(
+                content={"message": f"Reserva com código {codigo_reserva} não encontrada."},
+                status_code=404,
+            )
+
+        # Verifique se a poltrona está disponível
+        if not reserva.poltrona_disponivel(num_poltrona):
+            return JSONResponse(
+                content={"message": f"A poltrona {num_poltrona} não está disponível."},
+                status_code=400,
+            )
+
+        # Realize o check-in
+        reserva.realiza_checkin(num_poltrona)
+        session.commit()
+        
+        return JSONResponse(content={"message": "Check-in realizado com sucesso."})
+
+@reservas_router.patch("/{codigo_reserva}/checkin/{num_poltrona}")
+def atualiza_checkin(codigo_reserva: str, num_poltrona: int):
+    with get_session() as session:
+        reserva = session.exec(select(Reserva).where(Reserva.codigo_reserva == codigo_reserva)).first()
+
+        if not reserva:
+            return JSONResponse(
+                content={"message": f"Reserva com código {codigo_reserva} não encontrada."},
+                status_code=404,
+            )
+
+        # Verifique se a poltrona está disponível
+        if not reserva.poltrona_disponivel(num_poltrona):
+            return JSONResponse(
+                content={"message": f"A poltrona {num_poltrona} não está disponível."},
+                status_code=400,
+            )
+
+        # Atualize o check-in
+        reserva.atualiza_checkin(num_poltrona)
+        session.commit()
+        
+        return JSONResponse(content={"message": "Check-in atualizado com sucesso."})
